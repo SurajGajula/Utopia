78684e4da3000787afa35f96a70e7d5818d6c3f2
SurajGajula <ssamgajula@gmail.com>
2024-12-17 15:03:16 -0800

    id testing

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -2,7 +2,7 @@
 const cognitoConfig = {
     userPoolId: 'us-west-1_RAU6R6pD0',
     clientId: '45gfll4redstf4g8hq4fa2jkob',
-    domain: 'us-west-1rau6r6pd0',
+    domain: 'us-west-1rau6r6pd0',  // This needs to match exactly what's in your User Pool
     region: 'us-west-1',
     identityPoolId: 'us-west-1:be5f5c85-6e5f-421a-a20d-11f7b049b5d1'
 };
@@ -11,8 +11,8 @@ const cognitoConfig = {
 AWS.config.region = cognitoConfig.region;
 
 export function signIn() {
-    const redirectUri = encodeURIComponent('https://main.d22za2x5ln55me.amplifyapp.com/');
-    const loginUrl = `https://${cognitoConfig.domain}.auth.${cognitoConfig.region}.amazoncognito.com/login?client_id=${cognitoConfig.clientId}&redirect_uri=${redirectUri}&response_type=code&scope=email+openid`;
+    const redirectUri = 'https://main.d22za2x5ln55me.amplifyapp.com/';
+    const loginUrl = `https://${cognitoConfig.domain}.auth.${cognitoConfig.region}.amazoncognito.com/login?client_id=${cognitoConfig.clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=email+openid`;
     window.location.href = loginUrl;
 }
 
@@ -25,18 +25,24 @@ export async function handleCallback() {
             const redirectUri = 'https://main.d22za2x5ln55me.amplifyapp.com/';
             const tokenEndpoint = `https://${cognitoConfig.domain}.auth.${cognitoConfig.region}.amazoncognito.com/oauth2/token`;
             
-            const params = new URLSearchParams();
-            params.append('grant_type', 'authorization_code');
-            params.append('client_id', cognitoConfig.clientId);
-            params.append('code', code);
-            params.append('redirect_uri', redirectUri);
+            const body = new URLSearchParams({
+                grant_type: 'authorization_code',
+                code: code,
+                client_id: cognitoConfig.clientId,
+                redirect_uri: redirectUri
+            });
+
+            console.log('Token request:', {
+                url: tokenEndpoint,
+                body: body.toString()
+            });
 
             const response = await fetch(tokenEndpoint, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
-                body: params.toString()
+                body: body
             });
 
             if (!response.ok) {
