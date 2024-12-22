ce2dfedb113a90b691f13589894f38b16856cb57
SurajGajula <ssamgajula@gmail.com>
2024-12-21 17:29:47 -0800

    auth testing

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -1,3 +1,18 @@
+export const navigateToLogin = () => {
+    const cognitoConfig = {
+        cognitoDomain: 'https://us-west-1rau6r6pd0.auth.us-west-1.amazoncognito.com',
+        clientId: '45gfll4redstf4g8hq4fa2jkob',
+        redirectUri: 'https://main.d22za2x5ln55me.amplifyapp.com/',
+        responseType: 'code',
+        scope: 'email openid'
+    };
+    const loginUrl = `${cognitoConfig.cognitoDomain}/login/continue?` + 
+        `client_id=${cognitoConfig.clientId}&` +
+        `redirect_uri=${encodeURIComponent(cognitoConfig.redirectUri)}&` +
+        `response_type=${cognitoConfig.responseType}&` +
+        `scope=${cognitoConfig.scope.replace(' ', '+')}`;
+    window.location.href = loginUrl;
+};
 export const exchangeCodeForSub = async (code) => {
     try {
         const lambdaUrl = 'https://ynfalkk00f.execute-api.us-west-1.amazonaws.com/GetUID';
